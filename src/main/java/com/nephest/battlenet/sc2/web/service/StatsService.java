be88cbfb97276b9be3089b151819b78836398183
nephest <nephestdev@gmail.com>
2020-03-10 11:40:50 +0200

    add cache eviction

--- src/main/java/com/nephest/battlenet/sc2/web/service/StatsService.java
+++ src/main/java/com/nephest/battlenet/sc2/web/service/StatsService.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * =========================LICENSE_END=========================
@@ -101,6 +101,16 @@ public class StatsService
         this.statsService = statsService;
     }
 
+    @CacheEvict
+    (
+        cacheNames=
+        {
+            "search-seasons", "search-season-last",
+            "search-ladder", "search-ladder-stats",
+            "search-ladder-league-bounds", "search-ladder-season"
+        },
+        allEntries=true
+    )
     public void updateAll()
     {
         updateAll(true);
