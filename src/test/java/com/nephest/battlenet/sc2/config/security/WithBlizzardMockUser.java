efadd02cc2b94902a8d56cab889e4b109b3cd06c
Oleksandr Masniuk <nephestdev@gmail.com>
2020-05-12 17:42:58 +0300

    update license

--- src/test/java/com/nephest/battlenet/sc2/config/security/WithBlizzardMockUser.java
+++ src/test/java/com/nephest/battlenet/sc2/config/security/WithBlizzardMockUser.java
@@ -1,3 +1,6 @@
+// Copyright (C) 2020 Oleksandr Masniuk and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 package com.nephest.battlenet.sc2.config.security;
 
 import org.springframework.security.test.context.support.WithSecurityContext;
--- src/test/java/com/nephest/battlenet/sc2/model/local/dao/PlayerCharacterStatsDAOIT.java
+++ src/test/java/com/nephest/battlenet/sc2/model/local/dao/PlayerCharacterStatsDAOIT.java
@@ -1,3 +1,6 @@
+// Copyright (C) 2020 Oleksandr Masniuk and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 package com.nephest.battlenet.sc2.model.local.dao;
 
 import com.nephest.battlenet.sc2.config.DatabaseTestConfig;
