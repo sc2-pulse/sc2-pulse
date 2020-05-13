fb6efe6b269ca1bca1d5301bbee6bb30bd27e3ec
Oleksandr Masniuk <nephestdev@gmail.com>
2020-05-13 20:16:01 +0300

    add core test config

---
+++ src/test/java/com/nephest/battlenet/sc2/config/CoreTestConfig.java
@@ -0,0 +1,43 @@
+// Copyright (C) 2020 Oleksandr Masniuk and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package com.nephest.battlenet.sc2.config;
+
+import com.nephest.battlenet.sc2.config.convert.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+public class CoreTestConfig
+{
+    @Bean
+    public PlatformTransactionManager txManager(DataSource dataSource)
+    {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate sc2StatsNamedTemplate(DataSource dataSource)
+    {
+        return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public ConversionService sc2StatsConversionService()
+    {
+        DefaultFormattingConversionService service = new DefaultFormattingConversionService();
+        service.addConverter(new IdentifiableToIntegerConverter());
+        service.addConverter(new IntegerToQueueTypeConverter());
+        service.addConverter(new IntegerToLeagueTierTypeConverter());
+        service.addConverter(new IntegerToLeagueTypeConverter());
+        service.addConverter(new IntegerToRegionConverter());
+        service.addConverter(new IntegerToTeamTypeConverter());
+        service.addConverter(new IntegerToRaceConverter());
+        return service;
+    }
+}
--- src/test/java/com/nephest/battlenet/sc2/config/DatabaseTestConfig.java
+++ src/test/java/com/nephest/battlenet/sc2/config/DatabaseTestConfig.java
@@ -3,28 +3,19 @@
 
 package com.nephest.battlenet.sc2.config;
 
-
 import com.nephest.battlenet.sc2.Application;
-import com.nephest.battlenet.sc2.config.convert.*;
 import com.nephest.battlenet.sc2.config.security.SecurityConfig;
+import com.nephest.battlenet.sc2.web.service.StatsService;
+import com.nephest.battlenet.sc2.web.service.blizzard.BlizzardSC2API;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.format.support.DefaultFormattingConversionService;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @EnableCaching
 @EnableTransactionManagement
-@Configuration
 @EnableAutoConfiguration
 @ComponentScan
 (
@@ -32,34 +23,13 @@ import javax.sql.DataSource;
     excludeFilters =
     {
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Application.class),
-        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = SecurityConfig.class)
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = SecurityConfig.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = BlizzardSC2API.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = StatsService.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Cron.class)
     }
 )
+@Import(CoreTestConfig.class)
 public class DatabaseTestConfig
 {
-    @Bean
-    public PlatformTransactionManager txManager(DataSource dataSource)
-    {
-        return new DataSourceTransactionManager(dataSource);
-    }
-
-    @Bean
-    public NamedParameterJdbcTemplate sc2StatsNamedTemplate(DataSource dataSource)
-    {
-        return new NamedParameterJdbcTemplate(dataSource);
-    }
-
-    @Bean
-    public ConversionService sc2StatsConversionService()
-    {
-        DefaultFormattingConversionService service = new DefaultFormattingConversionService();
-        service.addConverter(new IdentifiableToIntegerConverter());
-        service.addConverter(new IntegerToQueueTypeConverter());
-        service.addConverter(new IntegerToLeagueTierTypeConverter());
-        service.addConverter(new IntegerToLeagueTypeConverter());
-        service.addConverter(new IntegerToRegionConverter());
-        service.addConverter(new IntegerToTeamTypeConverter());
-        service.addConverter(new IntegerToRaceConverter());
-        return service;
-    }
 }
