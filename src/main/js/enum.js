4e24e2d235e22a309d9eced72121962f1ebe1898
Oleksandr Masniuk <nephestdev@gmail.com>
2020-07-21 12:21:54 +0300

    add order property to enums

--- src/main/js/enum.js
+++ src/main/js/enum.js
@@ -3,44 +3,44 @@
 
 const REGION = Object.freeze
 ({
-    US: {code:1, name: "us"},
-    EU: {code:2, name: "eu"},
-    KR: {code:3, name: "kr"},
-    CN: {code:5, name: "cn"}
+    US: {code:1, name: "us", order: 1},
+    EU: {code:2, name: "eu", order: 2},
+    KR: {code:3, name: "kr", order: 3},
+    CN: {code:5, name: "cn", order: 4}
 });
 
 const RACE = Object.freeze
 ({
-    TERRAN: {name: "terran"},
-    PROTOSS: {name: "protoss"},
-    ZERG: {name: "zerg"},
-    RANDOM: {name: "random"}
+    TERRAN: {name: "terran", order: 1},
+    PROTOSS: {name: "protoss", order: 2},
+    ZERG: {name: "zerg", order: 3},
+    RANDOM: {name: "random", order: 4}
 });
 
 const LEAGUE = Object.freeze
 ({
-    BRONZE: {code:0, name: "bronze"},
-    SILVER: {code:1, name: "silver"},
-    GOLD: {code:2, name: "gold"},
-    PLATINUM: {code:3, name: "platinum"},
-    DIAMOND: {code:4, name: "diamond"},
-    MASTER: {code:5, name: "master"},
-    GRANDMASTER: {code:6, name: "grandmaster"}
+    BRONZE: {code:0, name: "bronze", order: 1},
+    SILVER: {code:1, name: "silver", order: 2},
+    GOLD: {code:2, name: "gold", order: 3},
+    PLATINUM: {code:3, name: "platinum", order: 4},
+    DIAMOND: {code:4, name: "diamond", order: 5},
+    MASTER: {code:5, name: "master", order: 6},
+    GRANDMASTER: {code:6, name: "grandmaster", order: 7}
 });
 
 const TEAM_FORMAT = Object.freeze
 ({
-    _1V1: {code:201, name: "1V1", fullName: "LOTV_1V1", memberCount: 1},
-    _2V2: {code:202, name: "2V2", fullName: "LOTV_2V2", memberCount: 2},
-    _3V3: {code:203, name: "3V3", fullName: "LOTV_3V3", memberCount: 3},
-    _4V4: {code:204, name: "4V4", fullName: "LOTV_4V4", memberCount: 4},
-    ARCHON: {code:206, name: "Archon", fullName: "LOTV_ARCHON", memberCount: 2}
+    _1V1: {code:201, name: "1V1", fullName: "LOTV_1V1", memberCount: 1, order: 1},
+    _2V2: {code:202, name: "2V2", fullName: "LOTV_2V2", memberCount: 2, order: 2},
+    _3V3: {code:203, name: "3V3", fullName: "LOTV_3V3", memberCount: 3, order: 3},
+    _4V4: {code:204, name: "4V4", fullName: "LOTV_4V4", memberCount: 4, order: 4},
+    ARCHON: {code:206, name: "Archon", fullName: "LOTV_ARCHON", memberCount: 2, order: 5}
 });
 
 const TEAM_TYPE = Object.freeze
 ({
-    ARRANGED: {code:0, name: "Arranged", fullName: "ARRANGED"},
-    RANDOM: {code:1, name: "Random", fullName: "RANDOM"}
+    ARRANGED: {code:0, name: "Arranged", fullName: "ARRANGED", order: 1},
+    RANDOM: {code:1, name: "Random", fullName: "RANDOM", order: 2}
 });
 
 const PAGE_TYPE = Object.freeze
