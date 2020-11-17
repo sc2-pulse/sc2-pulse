fc91e34865fdf473b3d6e6b5eb89d708ad1efb22
Oleksandr Masniuk <nephestdev@gmail.com>
2020-11-17 14:59:49 +0200

    remove SameSiteRememberMeAuthenticationSuccessfulHandler from db tests
    This fixes a ServletContext injection bug.

--- src/test/java/com/nephest/battlenet/sc2/config/DatabaseTestConfig.java
+++ src/test/java/com/nephest/battlenet/sc2/config/DatabaseTestConfig.java
@@ -5,6 +5,7 @@ package com.nephest.battlenet.sc2.config;
 
 import com.nephest.battlenet.sc2.Application;
 import com.nephest.battlenet.sc2.Startup;
+import com.nephest.battlenet.sc2.config.security.SameSiteRememberMeAuthenticationSuccessfulHandler;
 import com.nephest.battlenet.sc2.config.security.SecurityConfig;
 import com.nephest.battlenet.sc2.web.service.StatsService;
 import com.nephest.battlenet.sc2.web.service.blizzard.BlizzardSC2API;
@@ -24,6 +25,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Application.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = SecurityConfig.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = BlizzardSC2API.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = SameSiteRememberMeAuthenticationSuccessfulHandler.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = StatsService.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Cron.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = Startup.class)
