392209d9e0a74fd657faa511a420501ebdbfc4a5
Oleksandr Masniuk <nephestdev@gmail.com>
2020-12-08 09:46:19 +0200

    add standard dao

---
+++ src/main/java/com/nephest/battlenet/sc2/model/local/dao/StandardDAO.java
@@ -0,0 +1,28 @@
+// Copyright (C) 2020 Oleksandr Masniuk and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package com.nephest.battlenet.sc2.model.local.dao;
+
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import java.util.Collections;
+
+public class StandardDAO
+{
+
+    private final String REMOVE_EXPIRED_QUERY;
+    private final NamedParameterJdbcTemplate template;
+
+
+    public StandardDAO(NamedParameterJdbcTemplate template, String table, String period)
+    {
+        this.template = template;
+        REMOVE_EXPIRED_QUERY = String.format(DAOUtils.REMOVE_OUTDATED_TEMPLATE, table, period);
+    }
+
+    public int removeExpired()
+    {
+        return template.update(REMOVE_EXPIRED_QUERY, Collections.emptyMap());
+    }
+
+}
