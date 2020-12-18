feebeae82540d9a0436c4908fe20e113c75b572f
Oleksandr Masniuk <nephestdev@gmail.com>
2020-12-18 16:13:07 +0200

    move privacy policy to its own page

---
+++ src/main/java/com/nephest/battlenet/sc2/config/mvc/MVCConfig.java
@@ -0,0 +1,20 @@
+// Copyright (C) 2020 Oleksandr Masniuk and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package com.nephest.battlenet.sc2.config.mvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MVCConfig
+implements WebMvcConfigurer
+{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/privacy-policy").setViewName("privacy-policy");
+    }
+
+}
