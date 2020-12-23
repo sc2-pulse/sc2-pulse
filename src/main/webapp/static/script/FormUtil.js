3bd5067e3b8109f87ec0af9dd40852bddca06dfe
Oleksandr Masniuk <nephestdev@gmail.com>
2020-12-23 14:21:11 +0200

    add form utils

---
+++ src/main/webapp/static/script/FormUtil.js
@@ -0,0 +1,33 @@
+// Copyright (C) 2020 Oleksandr Masniuk and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+class FormUtil
+{
+
+    static getFormErrors(form)
+    {
+        const errors = [];
+        for(const cRow of form.querySelectorAll(":scope .group-checkbox"))
+        {
+            if(Array.from(cRow.querySelectorAll(':scope input[type="checkbox"]')).filter(c=>c.checked).length > 0) continue;
+
+            errors.push("You must select at least one " + cRow.getAttribute("data-name"));
+        }
+        return errors.join(". ");
+    }
+
+    static verifyForm(form, out)
+    {
+        const errors = FormUtil.getFormErrors(form);
+        if(!errors)
+        {
+            out.classList.add("d-none");
+            return true;
+        }
+
+        out.textContent = errors;
+        out.classList.remove("d-none");
+        return false;
+    }
+
+}
