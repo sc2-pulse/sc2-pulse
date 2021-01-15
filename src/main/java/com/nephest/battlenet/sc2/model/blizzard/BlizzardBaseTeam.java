9470c5ae4b6223306c71231e005f3dabf4890f22
Oleksandr Masniuk <nephestdev@gmail.com>
2021-01-15 17:35:52 +0200

    move setters to base class

---
+++ src/main/java/com/nephest/battlenet/sc2/model/blizzard/BlizzardBaseTeam.java
@@ -0,0 +1,77 @@
+// Copyright (C) 2020 Oleksandr Masniuk and contributors
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package com.nephest.battlenet.sc2.model.blizzard;
+
+import com.nephest.battlenet.sc2.model.BaseTeam;
+
+public class BlizzardBaseTeam
+extends BaseTeam
+{
+
+    public static final Long RATING_MAX = 20000L;
+
+    /*
+        blizzard normally returns zeros, not nulls.
+        in a very rare occasion some of the values could be null.
+        defaulting to zeros in this case
+    */
+    public BlizzardBaseTeam(){super(0L, 0, 0, 0, 0);}
+
+    public BlizzardBaseTeam
+    (
+        Long rating, Integer wins, Integer loses, Integer ties, Integer points
+    )
+    {
+        /*
+            blizzard normally returns zeros, not nulls.
+            in a very rare occasion some of the values could be nulls
+        */
+        super
+        (
+            rating,
+            (wins == null ? 0 : wins),
+            (loses == null ? 0 : loses),
+            (ties == null ? 0 : ties),
+            (points == null ? 0 : points)
+        );
+    }
+
+    //Blizzard can return unobtainable values here(cheaters?).
+    //Reducing them to zero to avoid ladder pollution
+    @Override
+    public void setRating(Long rating)
+    {
+        if(rating == null || rating > RATING_MAX) rating = 0L;
+        super.setRating(rating);
+    }
+
+    @Override
+    public void setWins(Integer wins)
+    {
+        if(wins == null) wins = 0;
+        super.setWins(wins);
+    }
+
+    @Override
+    public void setLosses(Integer losses)
+    {
+        if(losses == null) losses = 0;
+        super.setLosses(losses);
+    }
+
+    @Override
+    public void setTies(Integer ties)
+    {
+        if(ties == null) ties = 0;
+        super.setTies(ties);
+    }
+
+    @Override
+    public void setPoints(Integer points)
+    {
+        if(points == null) points = 0;
+        super.setPoints(points);
+    }
+
+}
--- src/main/java/com/nephest/battlenet/sc2/model/blizzard/BlizzardTeam.java
+++ src/main/java/com/nephest/battlenet/sc2/model/blizzard/BlizzardTeam.java
@@ -6,7 +6,6 @@ package com.nephest.battlenet.sc2.model.blizzard;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy.SnakeCaseStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import com.nephest.battlenet.sc2.model.BaseTeam;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
@@ -14,12 +13,10 @@ import java.math.BigInteger;
 
 @JsonNaming(SnakeCaseStrategy.class)
 public class BlizzardTeam
-extends BaseTeam
+extends BlizzardBaseTeam
 {
 
     private static final BlizzardTeamMember[] EMPTY_TEAM_MEMBER_ARRAY = new BlizzardTeamMember[0];
-
-    public static final Long RATING_MAX = 20000L;
 
     @NotNull
     private BigInteger id;
@@ -29,12 +26,7 @@ extends BaseTeam
     @JsonProperty("member")
     private BlizzardTeamMember[] members = EMPTY_TEAM_MEMBER_ARRAY;
 
-    /*
-        blizzard normally returns zeros, not nulls.
-        in a very rare occasion some of the values could be null.
-        defaulting to zeros in this case
-    */
-    public BlizzardTeam(){super(0L, 0, 0, 0, 0);}
+    public BlizzardTeam(){super();}
 
     public BlizzardTeam
     (
@@ -42,18 +34,7 @@ extends BaseTeam
         Long rating, Integer wins, Integer loses, Integer ties, Integer points
     )
     {
-        /*
-            blizzard normally returns zeros, not nulls.
-            in a very rare occasion some of the values could be nulls
-        */
-        super
-        (
-            rating,
-            (wins == null ? 0 : wins),
-            (loses == null ? 0 : loses),
-            (ties == null ? 0 : ties),
-            (points == null ? 0 : points)
-        );
+        super(rating, wins, loses, ties, points);
         this.id = id;
         this.members = members;
     }
@@ -76,43 +57,6 @@ extends BaseTeam
     public BlizzardTeamMember[] getMembers()
     {
         return members;
-    }
-
-    //Blizzard can return unobtainable values here(cheaters?).
-    //Reducing them to zero to avoid ladder pollution
-    @Override
-    public void setRating(Long rating)
-    {
-        if(rating == null || rating > RATING_MAX) rating = 0L;
-        super.setRating(rating);
-    }
-
-    @Override
-    public void setWins(Integer wins)
-    {
-        if(wins == null) wins = 0;
-        super.setWins(wins);
-    }
-
-    @Override
-    public void setLosses(Integer losses)
-    {
-        if(losses == null) losses = 0;
-        super.setLosses(losses);
-    }
-
-    @Override
-    public void setTies(Integer ties)
-    {
-        if(ties == null) ties = 0;
-        super.setTies(ties);
-    }
-
-    @Override
-    public void setPoints(Integer points)
-    {
-        if(points == null) points = 0;
-        super.setPoints(points);
     }
 
 }
