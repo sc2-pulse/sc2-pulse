53ed104f647dcb91afba5fcce8b52752bede332c
Oleksandr Masniuk <nephestdev@gmail.com>
2021-01-21 19:31:35 +0200

    add input timeout

--- src/main/webapp/static/script/ElementUtil.js
+++ src/main/webapp/static/script/ElementUtil.js
@@ -322,3 +322,4 @@ ElementUtil.INPUT_TIMEOUTS = new Map();
 ElementUtil.TITLE_CONSTRUCTORS = new Map();
 ElementUtil.DESCRIPTION_CONSTRUCTORS = new Map();
 ElementUtil.NEGATION_PREFIX = "neg-";
+ElementUtil.INPUT_TIMEOUT = 1000;
