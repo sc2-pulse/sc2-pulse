44b4416d9f60d52462cb94a612d0df3cdedcd668
Oleksandr Masniuk <nephestdev@gmail.com>
2021-02-08 12:58:50 +0200

    fix column name

--- src/main/java/com/nephest/battlenet/sc2/model/local/dao/MatchDAO.java
+++ src/main/java/com/nephest/battlenet/sc2/model/local/dao/MatchDAO.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Oleksandr Masniuk and contributors
+// Copyright (C) 2020-2021 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.model.local.dao;
@@ -60,7 +60,7 @@ extends StandardDAO
                 conversionService.convert(rs.getInt("match.type"), BaseMatch.MatchType.class),
                 rs.getString("match.map")
             );
-            match.setUpdated(rs.getObject("match.date", OffsetDateTime.class));
+            match.setUpdated(rs.getObject("match.updated", OffsetDateTime.class));
             return match;
         };
     }
