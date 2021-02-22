022e75fd66cbdbf15d7d378b376bdae317b06b8f
Oleksandr Masniuk <nephestdev@gmail.com>
2021-02-22 10:04:00 +0200

    decrease connection pool timers

--- src/main/java/com/nephest/battlenet/sc2/web/service/WebServiceUtil.java
+++ src/main/java/com/nephest/battlenet/sc2/web/service/WebServiceUtil.java
@@ -50,9 +50,9 @@ public class WebServiceUtil
         .transientErrors(true);
     public static final ConnectionProvider CONNECTION_PROVIDER = ConnectionProvider.builder("sc2-connection-provider")
         .maxConnections(700)
-        .maxIdleTime(Duration.ofMinutes(30))
-        .maxLifeTime(Duration.ofMinutes(90))
-        .evictInBackground(Duration.ofMinutes(10))
+        .maxIdleTime(Duration.ofMinutes(5))
+        .maxLifeTime(Duration.ofMinutes(30))
+        .evictInBackground(Duration.ofMinutes(5))
         .lifo()
         .build();
     public static final LoopResources LOOP_RESOURCES =
