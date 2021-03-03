5fbf0a2981a6482261030889ceff5e5b3dccb872
Oleksandr Masniuk <nephestdev@gmail.com>
2021-03-03 13:00:48 +0200

    clean revealed pro player name

--- src/main/java/com/nephest/battlenet/sc2/model/revealed/RevealedProPlayer.java
+++ src/main/java/com/nephest/battlenet/sc2/model/revealed/RevealedProPlayer.java
@@ -1,10 +1,11 @@
-// Copyright (C) 2020 Oleksandr Masniuk and contributors
+// Copyright (C) 2020-2021 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.model.revealed;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.nephest.battlenet.sc2.config.convert.jackson.SocialMediaMapDeserializer;
+import com.nephest.battlenet.sc2.config.convert.jackson.StdCleanStringDeserializer;
 import com.nephest.battlenet.sc2.model.SocialMedia;
 
 import javax.validation.constraints.NotNull;
@@ -19,6 +20,7 @@ public class RevealedProPlayer
     @NotNull
     private String _id;
 
+    @JsonDeserialize(using = StdCleanStringDeserializer.class)
     private String player;
 
     private String firstName;
