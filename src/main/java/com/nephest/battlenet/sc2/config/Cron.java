87a3f5485c2ec17d9ca7f41ae39b3208fefb61e9
Oleksandr Masniuk <nephestdev@gmail.com>
2021-04-21 16:23:16 +0300

    add executor

--- src/main/java/com/nephest/battlenet/sc2/config/Cron.java
+++ src/main/java/com/nephest/battlenet/sc2/config/Cron.java
@@ -15,6 +15,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
 
 @Profile({"!maintenance & !dev"})
@@ -43,6 +44,9 @@ public class Cron
 
     @Autowired
     private PostgreSQLUtils postgreSQLUtils;
+
+    @Autowired
+    private ThreadPoolTaskScheduler executor;
 
     @Scheduled(cron="0 0 7 * * *")
     public void updateSeasons()
