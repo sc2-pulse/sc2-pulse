8db339b0c8525ca48af44eb78f3e93d099f261fb
Oleksandr Masniuk <nephestdev@gmail.com>
2021-05-28 17:27:21 +0300

    update recently active character's matches

--- src/main/java/com/nephest/battlenet/sc2/web/service/MatchService.java
+++ src/main/java/com/nephest/battlenet/sc2/web/service/MatchService.java
@@ -11,6 +11,7 @@ import com.nephest.battlenet.sc2.model.local.PlayerCharacter;
 import com.nephest.battlenet.sc2.model.local.dao.MatchDAO;
 import com.nephest.battlenet.sc2.model.local.dao.MatchParticipantDAO;
 import com.nephest.battlenet.sc2.model.local.dao.PlayerCharacterDAO;
+import com.nephest.battlenet.sc2.model.local.dao.VarDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,10 @@ import reactor.util.function.Tuple2;
 import reactor.util.function.Tuple3;
 import reactor.util.function.Tuples;
 
+import javax.annotation.PostConstruct;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -38,6 +43,9 @@ public class MatchService
     private final MatchDAO matchDAO;
     private final MatchParticipantDAO matchParticipantDAO;
     private final PlayerCharacterDAO playerCharacterDAO;
+    private final VarDAO varDAO;
+
+    private Instant lastUpdated;
 
     @Autowired
     public MatchService
@@ -45,20 +53,57 @@ public class MatchService
         BlizzardSC2API api,
         PlayerCharacterDAO playerCharacterDAO,
         MatchDAO matchDAO,
-        MatchParticipantDAO matchParticipantDAO
+        MatchParticipantDAO matchParticipantDAO,
+        VarDAO varDAO
     )
     {
         this.api = api;
         this.playerCharacterDAO = playerCharacterDAO;
         this.matchDAO = matchDAO;
         this.matchParticipantDAO = matchParticipantDAO;
+        this.varDAO = varDAO;
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        //catch exceptions to allow service autowiring for tests
+        try {
+            loadLastUpdated();
+        }
+        catch(RuntimeException ex) {
+            LOG.warn(ex.getMessage(), ex);
+        }
+    }
+
+    private void loadLastUpdated()
+    {
+        String updatesVar = varDAO.find("match.updated").orElse(null);
+        if(updatesVar == null || updatesVar.isEmpty()) {
+            lastUpdated = null;
+            return;
+        }
+
+        lastUpdated = Instant.ofEpochMilli(Long.parseLong(updatesVar));
+        LOG.debug("Loaded last updated: {}", lastUpdated);
+    }
+
+    private void updateLastUpdated()
+    {
+        lastUpdated = Instant.now();
+        varDAO.merge("match.updated", String.valueOf(lastUpdated.toEpochMilli()));
     }
 
     @Transactional
     public void update()
     {
+        if(lastUpdated == null) {
+            updateLastUpdated();
+            return;
+        }
+
         AtomicInteger count = new AtomicInteger(0);
-        api.getMatches(playerCharacterDAO.findProPlayerCharacters())
+        api.getMatches(playerCharacterDAO.findRecentlyActiveCharacters(OffsetDateTime.ofInstant(lastUpdated, ZoneId.systemDefault())))
             .flatMap(m->Flux.fromArray(m.getT1().getMatches())
                 .zipWith(Flux.fromStream(Stream.iterate(m.getT2(), i->m.getT2()))))
             .buffer(BATCH_SIZE)
@@ -66,6 +111,7 @@ public class MatchService
             .toStream(2)
             .forEach(this::saveMatches);
         matchDAO.removeExpired();
+        updateLastUpdated();
         LOG.info("Saved {} matches", count.get());
     }
 
