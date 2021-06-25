8a6eeb9271f0c5f3231322a0451228ac89221674
Oleksandr Masniuk <nephestdev@gmail.com>
2021-06-25 12:49:07 +0300

    increase match identification time frame

--- src/main/java/com/nephest/battlenet/sc2/model/local/dao/MatchParticipantDAO.java
+++ src/main/java/com/nephest/battlenet/sc2/model/local/dao/MatchParticipantDAO.java
@@ -28,7 +28,7 @@ import java.util.stream.Collectors;
 public class MatchParticipantDAO
 {
     
-    public static final int IDENTIFICATION_FRAME_MINUTES = 25;
+    public static final int IDENTIFICATION_FRAME_MINUTES = 60;
 
     public static final String STD_SELECT =
         "match_participant.match_id AS \"match_participant.match_id\", "
