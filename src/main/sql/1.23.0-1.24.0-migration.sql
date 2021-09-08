fd8ee128b803503b67e75ab39ec2af0dcf9aac03
Oleksandr Masniuk <nephestdev@gmail.com>
2021-09-08 19:52:30 +0300

    remove order by id
    This makes team ranks fair. Multiple teams can share the same rank.

--- src/main/sql/1.23.0-1.24.0-migration.sql
+++ src/main/sql/1.23.0-1.24.0-migration.sql
@@ -18,6 +18,38 @@ ALTER TABLE "team_state"
     ADD COLUMN "global_team_count" INTEGER,
     ADD COLUMN "region_rank" INTEGER,
     ADD COLUMN "region_team_count" INTEGER;
+    
+WITH ranks AS
+(
+    SELECT id,
+    RANK() OVER(PARTITION BY queue_type, team_type ORDER BY rating DESC) as global_rank,
+    RANK() OVER(PARTITION BY queue_type, team_type, region ORDER BY rating DESC) as region_rank,
+    RANK() OVER(PARTITION BY queue_type, team_type, league_type ORDER BY rating DESC) as league_rank
+    FROM team
+    WHERE season < 40
+)
+UPDATE team
+set global_rank = ranks.global_rank,
+region_rank = ranks.region_rank,
+league_rank = ranks.league_rank
+FROM ranks
+WHERE team.id = ranks.id;
+
+WITH ranks AS
+(
+    SELECT id,
+    RANK() OVER(PARTITION BY queue_type, team_type ORDER BY rating DESC) as global_rank,
+    RANK() OVER(PARTITION BY queue_type, team_type, region ORDER BY rating DESC) as region_rank,
+    RANK() OVER(PARTITION BY queue_type, team_type, league_type ORDER BY rating DESC) as league_rank
+    FROM team
+    WHERE season >= 40
+)
+UPDATE team
+set global_rank = ranks.global_rank,
+region_rank = ranks.region_rank,
+league_rank = ranks.league_rank
+FROM ranks
+WHERE team.id = ranks.id;
 
 ALTER TABLE "team" SET (fillfactor = 50);
 VACUUM FULL "team";
