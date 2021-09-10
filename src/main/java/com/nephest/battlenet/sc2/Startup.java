339ed65aa7e9c4b5c4c03c421615a579ec7bf983
Oleksandr Masniuk <nephestdev@gmail.com>
2021-09-10 11:44:14 +0300

    add timeout properties

--- src/main/java/com/nephest/battlenet/sc2/Startup.java
+++ src/main/java/com/nephest/battlenet/sc2/Startup.java
@@ -3,20 +3,22 @@
 
 package com.nephest.battlenet.sc2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.concurrent.Executor;
 
-@Profile({"!maintenance & !dev"})
 @Component
 public class Startup
 implements ApplicationRunner
 {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Startup.class);
 
     @Autowired @Qualifier("taskScheduler")
     private Executor executor;
@@ -24,5 +26,14 @@ implements ApplicationRunner
     @Override
     public void run(ApplicationArguments args)
     {
+        //Spring uses internal RestTemplates in oauth client. These templates can't be configured via external config.
+        if
+        (
+            System.getProperty("sun.net.client.defaultConnectTimeout") == null
+            || Integer.parseInt(System.getProperty("sun.net.client.defaultConnectTimeout")) < 1
+            || System.getProperty("sun.net.client.defaultReadTimeout") == null
+            || Integer.parseInt(System.getProperty("sun.net.client.defaultReadTimeout")) < 1
+        )
+        LOG.warn("sun.net.client IO timeouts are not set. The application can block indefinitely without the timeouts");
     }
 }
