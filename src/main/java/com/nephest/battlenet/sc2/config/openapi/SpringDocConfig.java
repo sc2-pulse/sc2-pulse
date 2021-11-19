3a89978b2545bad907293a6f702e8ac91d184c20
Oleksandr Masniuk <nephestdev@gmail.com>
2021-11-19 11:47:23 +0200

    Clarify API info
    Blizzard ToS forbids any data transfer/sublicense, but they can't forbid REST API simply because this is how modern web apps work. These changes fulfill the ToS requirements.

--- src/main/java/com/nephest/battlenet/sc2/config/openapi/SpringDocConfig.java
+++ src/main/java/com/nephest/battlenet/sc2/config/openapi/SpringDocConfig.java
@@ -6,6 +6,7 @@ package com.nephest.battlenet.sc2.config.openapi;
 import com.nephest.battlenet.sc2.Application;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -24,10 +25,10 @@ public class SpringDocConfig
             .info(new Info()
                 .title("SC2 Pulse API")
                 .version(Application.VERSION)
-                .description("You are free to use the API for non-commercial purposes if you credit the original "
-                    + "website(nephest.com/sc2). API endpoints are pretty self explanatory, there are no special params or "
-                    + "hidden points, anything you can use is already used by the website, so grab whatever data you like. "
-                    + "Just be reasonable with your request rate."));
+                .license(new License()
+                    .name("Blizzard Developer API Terms Of Use")
+                    .url("https://www.blizzard.com/en-us/legal/a2989b50-5f16-43b1-abec-2ae17cc09dd6/blizzard-developer-api-terms-of-use"))
+                .description("It would be nice if you credit the original website(nephest.com/sc2)"));
     }
 
 }
