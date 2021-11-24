3d1c2bc8af7caf8b67a435b1c2b892b67839adb2
Oleksandr Masniuk <nephestdev@gmail.com>
2021-11-24 16:46:48 +0200

    add name

--- src/main/java/com/nephest/battlenet/sc2/model/Region.java
+++ src/main/java/com/nephest/battlenet/sc2/model/Region.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2020 Oleksandr Masniuk and contributors
+// Copyright (C) 2020-2021 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.model;
@@ -7,17 +7,19 @@ public enum Region
 implements Identifiable
 {
 
-    US(1, "https://us.api.blizzard.com/"),
-    EU(2, "https://eu.api.blizzard.com/"),
-    KR(3, "https://kr.api.blizzard.com/"),
-    CN(5, "https://gateway.battlenet.com.cn/");
+    US(1, "NA", "https://us.api.blizzard.com/"),
+    EU(2, "EU", "https://eu.api.blizzard.com/"),
+    KR(3, "KR", "https://kr.api.blizzard.com/"),
+    CN(5, "CN", "https://gateway.battlenet.com.cn/");
 
     private final int id;
+    private final String name;
     private final String baseUrl;
 
-    Region(int id, String baseUrl)
+    Region(int id, String name, String baseUrl)
     {
         this.id = id;
+        this.name = name;
         this.baseUrl = baseUrl;
     }
 
@@ -35,6 +37,11 @@ implements Identifiable
     public int getId()
     {
         return id;
+    }
+
+    public String getName()
+    {
+        return name;
     }
 
     public String getBaseUrl()
