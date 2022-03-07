8fafbf8f421f8514f2867f52404fca505b587528
Oleksandr Masniuk <nephestdev@gmail.com>
2022-03-07 12:17:02 +0200

    add java time module

---
+++ src/main/java/com/nephest/battlenet/sc2/config/convert/jackson/JacksonConfig.java
@@ -0,0 +1,20 @@
+// Copyright (C) 2020-2022 Oleksandr Masniuk
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package com.nephest.battlenet.sc2.config.convert.jackson;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig
+{
+
+    @Bean
+    public JavaTimeModule javaTimeModule()
+    {
+        return new JavaTimeModule();
+    }
+
+}
