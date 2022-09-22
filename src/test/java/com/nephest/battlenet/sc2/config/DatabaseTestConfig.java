15d00aa2f96556a2980d2f12379b143cf70d2e6f
Oleksandr Masniuk <nephestdev@gmail.com>
2022-09-22 12:58:52 +0300

    exclude ClanService
    This fixes a bug where Spring context can't be loaded due to missing dependencies.

--- src/test/java/com/nephest/battlenet/sc2/config/DatabaseTestConfig.java
+++ src/test/java/com/nephest/battlenet/sc2/config/DatabaseTestConfig.java
@@ -18,6 +18,7 @@ import com.nephest.battlenet.sc2.web.controller.TeamController;
 import com.nephest.battlenet.sc2.web.service.AlternativeLadderService;
 import com.nephest.battlenet.sc2.web.service.BlizzardPrivacyService;
 import com.nephest.battlenet.sc2.web.service.BlizzardSC2API;
+import com.nephest.battlenet.sc2.web.service.ClanService;
 import com.nephest.battlenet.sc2.web.service.MatchService;
 import com.nephest.battlenet.sc2.web.service.SC2WebServiceUtil;
 import com.nephest.battlenet.sc2.web.service.StatsService;
@@ -43,6 +44,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = BlizzardSC2API.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = BlizzardPrivacyService.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = StatsService.class),
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ClanService.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = SC2WebServiceUtil.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = StatusService.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = AlternativeLadderService.class),
