3df057c23efe40bed2ac576adf890f44fa974dc5
Oleksandr Masniuk <nephestdev@gmail.com>
2022-10-11 10:54:32 +0300

    rename login view
    This fixes a name clash with buil-in sba server.

--- src/main/java/com/nephest/battlenet/sc2/config/mvc/MVCConfig.java
+++ src/main/java/com/nephest/battlenet/sc2/config/mvc/MVCConfig.java
@@ -15,7 +15,7 @@ implements WebMvcConfigurer
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login").setViewName("plogin");
         registry.addViewController("/terms-of-service").setViewName("terms-of-service");
         registry.addViewController("/privacy-policy").setViewName("privacy-policy");
         registry.addViewController("/about").setViewName("about");
