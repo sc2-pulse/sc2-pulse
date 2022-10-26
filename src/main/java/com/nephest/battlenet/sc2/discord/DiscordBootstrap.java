dcea83c7cff45f57c40ff5472fb3df67fc5504da
Oleksandr Masniuk <nephestdev@gmail.com>
2022-10-26 17:55:29 +0300

    make "import BattleNet data" a discord link instead of url

--- src/main/java/com/nephest/battlenet/sc2/discord/DiscordBootstrap.java
+++ src/main/java/com/nephest/battlenet/sc2/discord/DiscordBootstrap.java
@@ -86,7 +86,7 @@ public class DiscordBootstrap
     private final Map<BaseLeague.LeagueType, String> leagueEmojis;
     private final String characterUrlTemplate;
     private final String accountVerificationLink;
-    private final String importBattleNetDataUrl;
+    private final String importBattleNetDataLink;
     private final GuildEmojiStore guildEmojiStore;
     private final UpdateService updateService;
 
@@ -108,7 +108,10 @@ public class DiscordBootstrap
             "[verify your account](<"
             + webContextUtil.getPublicUrl()
             + "verify/discord>)";
-        this.importBattleNetDataUrl = webContextUtil.getPublicUrl() + "data/battle-net";
+        this.importBattleNetDataLink =
+            "[import your BattleNet profile](<"
+            + webContextUtil.getPublicUrl()
+            + "data/battle-net>)";
         this.guildEmojiStore = guildEmojiStore;
         this.updateService = updateService;
     }
@@ -304,9 +307,9 @@ public class DiscordBootstrap
         return accountVerificationLink;
     }
 
-    public String getImportBattleNetDataUrl()
+    public String getImportBattleNetDataLink()
     {
-        return importBattleNetDataUrl;
+        return importBattleNetDataLink;
     }
 
     public static String generateFullName(LadderTeamMember member, boolean boldName)
