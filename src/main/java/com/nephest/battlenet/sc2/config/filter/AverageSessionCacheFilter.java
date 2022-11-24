08a89844142515f6a2bf629c5f08ea78c1447332
Oleksandr Masniuk <nephestdev@gmail.com>
2022-11-24 12:13:32 +0200

    tighten cache restrictions

--- src/main/java/com/nephest/battlenet/sc2/config/filter/AverageSessionCacheFilter.java
+++ src/main/java/com/nephest/battlenet/sc2/config/filter/AverageSessionCacheFilter.java
@@ -1,13 +1,17 @@
-// Copyright (C) 2020-2021 Oleksandr Masniuk
+// Copyright (C) 2020-2022 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.config.filter;
 
-import org.springframework.core.annotation.Order;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
 import java.time.Duration;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
 
 @Order(-1) //pre-security order
 public class AverageSessionCacheFilter
@@ -15,14 +19,16 @@ implements Filter
 {
 
     public static final Duration CACHE_DURATION = Duration.ofSeconds(240);
-    private static final String MAX_AGE = "max-age=" + CACHE_DURATION.toSeconds();
+    private static final String HEADER_VALUE = "private, "
+        + "max-age=" + CACHE_DURATION.toSeconds() + ", "
+        + "must-revalidate";
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
     throws java.io.IOException, ServletException
     {
         HttpServletResponse hresp = (HttpServletResponse) resp;
-        hresp.setHeader("Cache-Control", MAX_AGE);
+        hresp.setHeader("Cache-Control", HEADER_VALUE);
         chain.doFilter(req, resp);
     }
 
