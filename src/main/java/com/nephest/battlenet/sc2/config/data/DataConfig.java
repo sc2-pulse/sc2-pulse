fa6df9c66dcc2d965f28b080931e234699b51561
Oleksandr Masniuk <nephestdev@gmail.com>
2022-12-02 12:41:04 +0200

    move data config to a proper package

--- src/main/java/com/nephest/battlenet/sc2/config/data/DataConfig.java
+++ src/main/java/com/nephest/battlenet/sc2/config/data/DataConfig.java
@@ -1,7 +1,7 @@
 // Copyright (C) 2020-2022 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-package com.nephest.battlenet.sc2.config.filter;
+package com.nephest.battlenet.sc2.config.data;
 
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
