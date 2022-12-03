8156169c294a21d7f45f218cec2f23c22ee30837
Oleksandr Masniuk <nephestdev@gmail.com>
2022-12-03 07:42:12 +0200

    add static no-cache header

--- src/main/java/com/nephest/battlenet/sc2/config/filter/NoCacheFilter.java
+++ src/main/java/com/nephest/battlenet/sc2/config/filter/NoCacheFilter.java
@@ -1,9 +1,13 @@
-// Copyright (C) 2020-2021 Oleksandr Masniuk
+// Copyright (C) 2020-2022 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.config.filter;
 
-import javax.servlet.*;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,12 +16,15 @@ import javax.servlet.http.HttpServletResponse;
 public class NoCacheFilter
 implements Filter
 {
+
+    public static final String NO_CACHE_HEADER = "no-cache, no-store, must-revalidate";
+
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
     throws java.io.IOException, ServletException
     {
         HttpServletResponse hresp = (HttpServletResponse) resp;
-        hresp.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+        hresp.setHeader("Cache-Control", NO_CACHE_HEADER);
         chain.doFilter(req, resp);
     }
 }
