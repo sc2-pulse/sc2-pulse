81ba201961a8b87e92b3d673ced44e7137ea0846
Oleksandr Masniuk <nephestdev@gmail.com>
2023-05-01 17:51:39 +0300

    improve security test

--- src/test/java/com/nephest/battlenet/sc2/web/service/SecurityIT.java
+++ src/test/java/com/nephest/battlenet/sc2/web/service/SecurityIT.java
@@ -1,50 +1,52 @@
-// Copyright (C) 2020-2022 Oleksandr Masniuk
+// Copyright (C) 2020-2023 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.web.service;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.nephest.battlenet.sc2.config.AllTestConfig;
 import com.nephest.battlenet.sc2.config.security.SC2PulseAuthority;
 import com.nephest.battlenet.sc2.config.security.WithBlizzardMockUser;
 import com.nephest.battlenet.sc2.model.Partition;
+import com.nephest.battlenet.sc2.model.local.Account;
+import com.nephest.battlenet.sc2.model.local.dao.AccountDAO;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(classes = {AllTestConfig.class})
 @ActiveProfiles({"default", "prod", "test"})
+@AutoConfigureMockMvc
 @TestPropertySource("classpath:application.properties")
 @TestPropertySource("classpath:application-private.properties")
 public class SecurityIT
 {
 
-    private static MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
     @BeforeAll
     public static void init
     (
         @Autowired DataSource dataSource,
-        @Autowired WebApplicationContext webApplicationContext
+        @Autowired AccountDAO accountDAO
     ) throws SQLException
     {
         try (Connection connection = dataSource.getConnection())
@@ -52,79 +54,95 @@ public class SecurityIT
             ScriptUtils.executeSqlScript(connection, new ClassPathResource("schema-drop-postgres.sql"));
             ScriptUtils.executeSqlScript(connection, new ClassPathResource("schema-postgres.sql"));
         }
-        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity())
-            .alwaysDo(print()).build();
+        accountDAO.merge(new Account(1L, Partition.GLOBAL, "user"));
+    }
+
+    @AfterAll
+    public static void afterAll(@Autowired DataSource dataSource)
+    throws SQLException
+    {
+        try (Connection connection = dataSource.getConnection())
+        {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("schema-drop-postgres.sql"));
+        }
     }
 
 
     @ParameterizedTest
     @CsvSource
     ({
-        "/verify/discord, false",
+        "GET, /verify/discord, 401",
     })
-    public void testNotAuthenticatedSecurity(String path, boolean allowed)
+    public void testNotAuthenticatedSecurity(HttpMethod method, String path, int status)
     throws Exception
     {
         mvc.perform
         (
-            get(path)
+            request(method, path)
                 .with(csrf().asHeader())
-        ).andExpect(allowed ? status().isOk() : status().isUnauthorized());
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"/admin", "/sba"})
-    @WithBlizzardMockUser(partition =  Partition.GLOBAL, username = "user", roles = {SC2PulseAuthority.USER})
-    public void testUserSecurity(String path) throws Exception
-    {
-        mvc.perform
-        (
-            get(path)
-                .contentType(MediaType.APPLICATION_JSON)
-                .with(csrf().asHeader())
-        ).andExpect(status().isForbidden());
+        ).andExpect(status().is(status));
     }
 
     @ParameterizedTest
     @CsvSource
     ({
-        "/admin, false",
-        "/sba/instances/1/actuator/loggers, false",
-        "/sba/actuator/info, false",
+        "GET, /admin, 403",
+        "GET, /sba, 403"
+    })
+    @WithBlizzardMockUser(partition =  Partition.GLOBAL, username = "user", roles = {SC2PulseAuthority.USER})
+    public void testUserSecurity(HttpMethod method, String path, int status)
+    throws Exception
+    {
+        mvc.perform
+        (
+            request(method, path)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf().asHeader())
+        ).andExpect(status().is(status));
+    }
 
-        "/sba, true",
-        "/sba/instances, true",
-        "/sba/instances/1, true",
-        "/sba/instances/1/actuator/info, true"
+    @ParameterizedTest
+    @CsvSource
+    ({
+        "GET, /admin, 403",
+        "GET, /sba/instances/1/actuator/loggers, 403",
+        "GET, /sba/actuator/info, 403",
+
+        //not found instead of ok because SBA server is not deployed in this test
+        "GET, /sba, 404",
+        "GET, /sba/instances, 404",
+        "GET, /sba/instances/1, 404",
+        "GET, /sba/instances/1/actuator/info, 404"
     })
     @WithBlizzardMockUser(partition =  Partition.GLOBAL, username = "user", roles = {SC2PulseAuthority.SERVER_WATCHER})
-    public void testServerWatcherSecurity(String path, boolean allowed) throws Exception
+    public void testServerWatcherSecurity(HttpMethod method, String path, int status)
+    throws Exception
     {
         mvc.perform
         (
-            get(path)
-            .contentType(MediaType.APPLICATION_JSON)
-            .with(csrf().asHeader())
-        //not found instead of ok because SBA server is not deployed in this test
-        ).andExpect(allowed ? status().isNotFound() : status().isForbidden());
+            request(method, path)
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf().asHeader())
+        ).andExpect(status().is(status));
     }
 
     @ParameterizedTest
     @CsvSource
     ({
-        "/admin, true",
-        "/sba/instances/1/actuator/loggers, true",
+        //not found instead of ok because SBA server is not deployed in this test
+        "GET, /admin, 404",
+        "GET, /sba/instances/1/actuator/loggers, 404",
     })
     @WithBlizzardMockUser(partition =  Partition.GLOBAL, username = "user", roles = {SC2PulseAuthority.ADMIN})
-    public void testAdminSecurity(String path, boolean allowed) throws Exception
+    public void testAdminSecurity(HttpMethod method, String path, int status)
+    throws Exception
     {
         mvc.perform
         (
-            get(path)
+            request(method, path)
                 .contentType(MediaType.APPLICATION_JSON)
                 .with(csrf().asHeader())
-        //not found instead of ok because SBA server is not deployed in this test
-        ).andExpect(allowed ? status().isNotFound() : status().isForbidden());
+        ).andExpect(status().is(status));
     }
 
 }
