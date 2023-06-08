92aaa546cb862aea5cf58bca3d81802e6ba8ef07
Oleksandr Masniuk <nephestdev@gmail.com>
2023-06-08 17:22:23 +0300

    add reset function

--- src/main/webapp/static/script/Model.js
+++ src/main/webapp/static/script/Model.js
@@ -9,6 +9,12 @@ class Model
         for(const view of Object.values(VIEW)) Model.DATA.set(view, new Map());
     }
 
+    static reset(view, viewDatas = Object.values(VIEW_DATA))
+    {
+        const viewObj = Model.DATA.get(view);
+        for(const viewData of viewDatas) viewObj.set(viewData, {});
+    }
+
 }
 
 Model.DATA = new Map();
