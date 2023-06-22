43b392fbdd06ea8578c63ee7e30385ee35ea69a5
Oleksandr Masniuk <nephestdev@gmail.com>
2023-06-22 12:29:46 +0300

    rearrange operations

--- src/main/sql/1.53.0-1.54.0-migration.sql
+++ src/main/sql/1.53.0-1.54.0-migration.sql
@@ -1,8 +1,8 @@
-CREATE UNIQUE INDEX "uq_discord_user_name_discriminator" ON "discord_user"("name", "discriminator") WHERE "discriminator" IS NOT NULL;
-CREATE UNIQUE INDEX "uq_discord_user_name" ON "discord_user"("name") WHERE "discriminator" IS NULL;
 ALTER TABLE "discord_user"
     ALTER COLUMN "discriminator" DROP NOT NULL,
     DROP CONSTRAINT "uq_discord_user_name_discriminator";
+CREATE UNIQUE INDEX "uq_discord_user_name_discriminator" ON "discord_user"("name", "discriminator") WHERE "discriminator" IS NOT NULL;
+CREATE UNIQUE INDEX "uq_discord_user_name" ON "discord_user"("name") WHERE "discriminator" IS NULL;
 
 UPDATE "discord_user"
 SET "discriminator" = NULL
