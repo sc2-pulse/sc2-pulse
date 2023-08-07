4fa3ea64b618c04cad63cce196b59b6d476f6dc7
Oleksandr Masniuk <nephestdev@gmail.com>
2023-08-07 17:20:07 +0300

    update license

--- src/main/java/com/nephest/battlenet/sc2/config/openapi/SpringDocConfig.java
+++ src/main/java/com/nephest/battlenet/sc2/config/openapi/SpringDocConfig.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2021 Oleksandr Masniuk
+// Copyright (C) 2020-2023 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.config.openapi;
@@ -8,11 +8,10 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import javax.servlet.ServletContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.servlet.ServletContext;
 
 @Configuration
 public class SpringDocConfig
@@ -28,7 +27,8 @@ public class SpringDocConfig
                 .license(new License()
                     .name("Blizzard Developer API Terms Of Use")
                     .url("https://www.blizzard.com/en-us/legal/a2989b50-5f16-43b1-abec-2ae17cc09dd6/blizzard-developer-api-terms-of-use"))
-                .description("It would be nice if you credit the original website(nephest.com/sc2)"));
+                .description("You are free to use this web API for non-commercial purposes if you "
+                    + "credit the original website(https://sc2pulse.nephest.com/sc2)"));
     }
 
 }
