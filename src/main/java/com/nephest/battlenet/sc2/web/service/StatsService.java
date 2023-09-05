0c8dd51f057e7527892f9d83abf394690a091649
Oleksandr Masniuk <nephestdev@gmail.com>
2023-09-05 17:19:37 +0300

    remove transaction

--- src/main/java/com/nephest/battlenet/sc2/web/service/StatsService.java
+++ src/main/java/com/nephest/battlenet/sc2/web/service/StatsService.java
@@ -347,7 +347,6 @@ public class StatsService
             statsService.afterCurrentSeasonUpdate(allStats, pending, altPending));
     }
 
-    @Transactional
     public void afterCurrentSeasonUpdate
     (
         boolean allStats,
