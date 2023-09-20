fe365b1da0c4401ce37d18409e2a3c3459970bbb
Oleksandr Masniuk <nephestdev@gmail.com>
2023-09-20 16:47:48 +0300

    use user ids instead of logins

--- src/main/java/com/nephest/battlenet/sc2/web/service/TwitchService.java
+++ src/main/java/com/nephest/battlenet/sc2/web/service/TwitchService.java
@@ -7,6 +7,7 @@ import com.github.twitch4j.TwitchClient;
 import com.github.twitch4j.helix.domain.User;
 import com.github.twitch4j.helix.domain.Video;
 import com.nephest.battlenet.sc2.model.SocialMedia;
+import com.nephest.battlenet.sc2.model.local.SocialMediaLink;
 import com.nephest.battlenet.sc2.model.local.dao.MatchDAO;
 import com.nephest.battlenet.sc2.model.local.dao.MatchParticipantDAO;
 import com.nephest.battlenet.sc2.model.local.dao.ProPlayerDAO;
@@ -19,6 +20,7 @@ import com.nephest.battlenet.sc2.service.EventService;
 import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -88,16 +90,17 @@ public class TwitchService
 
     private void updateTwitchData()
     {
-        List<String> twitchLogins = socialMediaLinkDAO.findByTypes(SocialMedia.TWITCH).stream()
-            .map(l->l.getUrl().substring(l.getUrl().lastIndexOf("/") + 1))
+        List<String> twitchIds = socialMediaLinkDAO.findByTypes(SocialMedia.TWITCH).stream()
+            .map(SocialMediaLink::getServiceUserId)
+            .filter(Objects::nonNull)
             .collect(Collectors.toList());
-        if(twitchLogins.isEmpty()) return;
+        if(twitchIds.isEmpty()) return;
 
         int batchIx = 0;
-        while(batchIx < twitchLogins.size())
+        while(batchIx < twitchIds.size())
         {
-            List<String> batch = twitchLogins.subList(batchIx, Math.min(batchIx + USER_BATCH_SIZE, twitchLogins.size()));
-            List<User> userBatch = twitchClient.getHelix().getUsers(null, null, batch)
+            List<String> batch = twitchIds.subList(batchIx, Math.min(batchIx + USER_BATCH_SIZE, twitchIds.size()));
+            List<User> userBatch = twitchClient.getHelix().getUsers(null, batch, null)
                 .execute().getUsers();
             List<TwitchUser> localUserBatch = userBatch.stream()
                 .map(TwitchUser::of)
@@ -105,7 +108,7 @@ public class TwitchService
             twitchUserDAO.merge(localUserBatch.toArray(TwitchUser[]::new));
             userBatch.forEach(this::updateVideos);
             batchIx += batch.size();
-            LOG.trace("Twitch users progress: {}/{} ", batchIx, twitchLogins.size());
+            LOG.trace("Twitch users progress: {}/{} ", batchIx, twitchIds.size());
         }
         twitchVideoDAO.removeExpired();
     }
