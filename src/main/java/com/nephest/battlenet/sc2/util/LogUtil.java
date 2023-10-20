6939749ea58d266b4023ad8ceee538f387e8d863
Oleksandr Masniuk <nephestdev@gmail.com>
2023-10-20 17:08:49 +0300

    add trace log

--- src/main/java/com/nephest/battlenet/sc2/util/LogUtil.java
+++ src/main/java/com/nephest/battlenet/sc2/util/LogUtil.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2021 Oleksandr Masniuk
+// Copyright (C) 2020-2023 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.util;
@@ -24,6 +24,18 @@ public final class LogUtil
             case INFO: log.info(template, params); break;
             case WARNING: log.warn(template, params); break;
             case ERROR: log.error(template, params); break;
+        }
+    }
+
+    public static void log(Logger log, LogLevel level, Throwable throwable)
+    {
+        switch (level)
+        {
+            case TRACE: log.trace(throwable.getMessage(), throwable); break;
+            case DEBUG: log.debug(throwable.getMessage(), throwable); break;
+            case INFO: log.info(throwable.getMessage(), throwable); break;
+            case WARNING: log.warn(throwable.getMessage(), throwable); break;
+            case ERROR: log.error(throwable.getMessage(), throwable); break;
         }
     }
 
