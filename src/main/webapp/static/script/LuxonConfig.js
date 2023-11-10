b7fd16d0f75f414ea2b7012aabfbdb689af1c122
Oleksandr Masniuk <nephestdev@gmail.com>
2023-11-10 10:41:50 +0200

    add missing keyword

--- src/main/webapp/static/script/LuxonConfig.js
+++ src/main/webapp/static/script/LuxonConfig.js
@@ -1,5 +1,5 @@
 luxon.Duration.prototype.toLargestUnitString = function(unitNameLen = 3) {
-    shifted = this.shiftTo("years", "month", "days", "hours", "minutes", "seconds", "milliseconds").toObject();
+    const shifted = this.shiftTo("years", "month", "days", "hours", "minutes", "seconds", "milliseconds").toObject();
     const largest = Object.entries(shifted).find(e=>e[1] > 0);
 
     return largest ? (largest[1] + " " + largest[0].substring(0, Math.min(unitNameLen, largest[0].length))) : "recent";
