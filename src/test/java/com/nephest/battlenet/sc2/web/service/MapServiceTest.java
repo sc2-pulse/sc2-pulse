75a0a8321f6244566c79c99def30972a53daabad
Oleksandr Masniuk <nephestdev@gmail.com>
2024-01-04 14:45:33 +0200

    add missing subscription

--- src/test/java/com/nephest/battlenet/sc2/web/service/MapServiceTest.java
+++ src/test/java/com/nephest/battlenet/sc2/web/service/MapServiceTest.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2023 Oleksandr Masniuk
+// Copyright (C) 2020-2024 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.web.service;
@@ -6,6 +6,9 @@ package com.nephest.battlenet.sc2.web.service;
 import static com.nephest.battlenet.sc2.web.service.MapService.MAP_STATS_SKIP_NEW_SEASON_FRAME;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.nephest.battlenet.sc2.model.Region;
@@ -14,7 +17,9 @@ import com.nephest.battlenet.sc2.model.local.dao.MapStatsDAO;
 import com.nephest.battlenet.sc2.model.local.dao.SeasonDAO;
 import com.nephest.battlenet.sc2.model.local.dao.VarDAO;
 import com.nephest.battlenet.sc2.service.EventService;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Map;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -65,6 +70,26 @@ public class MapServiceTest
         when(seasonDAO.findLast())
             .thenReturn(Optional.of(new Season(1, 1, Region.EU, 2020, 1, youngDate, youngDate.plusDays(1))));
         assertTrue(mapService.seasonIsTooYoung());
+    }
+
+    @Test
+    public void whenCreatingBean_thenUpdateOnMatchUpdateEvent()
+    throws InterruptedException
+    {
+        LocalDate oldDate = LocalDate.now().minusDays(MAP_STATS_SKIP_NEW_SEASON_FRAME.toDays());
+        when(seasonDAO.findLast())
+            .thenReturn(Optional.of(new Season(1, 1, Region.EU, 2020, 1, oldDate, oldDate.plusDays(1))));
+
+        MatchUpdateContext ctx = new MatchUpdateContext
+        (
+            Map.of(),
+            new UpdateContext(Instant.now(), Instant.now())
+        );
+        when(eventService.getMatchUpdateEvent()).thenReturn(Flux.just(ctx, ctx));
+        mapService = new MapService(seasonDAO, mapStatsDAO, varDAO, eventService);
+
+        Thread.sleep(100);
+        verify(mapStatsDAO, times(2)).add(any(), any());
     }
 
 }
