27a786783318c7bbd33dcdb3c8efc3aeaad3d2b7
Oleksandr Masniuk <nephestdev@gmail.com>
2024-01-16 17:06:27 +0200

    allow null teams

--- src/test/java/com/nephest/battlenet/sc2/web/service/community/CommunityVideoStreamIT.java
+++ src/test/java/com/nephest/battlenet/sc2/web/service/community/CommunityVideoStreamIT.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2023 Oleksandr Masniuk
+// Copyright (C) 2020-2024 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.web.service.community;
@@ -573,7 +573,7 @@ public class CommunityVideoStreamIT
             ),
             ladderSearchDAO.findCharacterTeams(Set.of(ix + 1L)).stream()
                 .findAny()
-                .orElseThrow(),
+                .orElse(null),
             featured
         );
     }
