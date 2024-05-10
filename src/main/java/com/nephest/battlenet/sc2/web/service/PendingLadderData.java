872f59eff589f4bcb8b745aee3450c8c689dd6e8
Oleksandr Masniuk <nephestdev@gmail.com>
2024-05-10 14:53:44 +0300

    add immutable copy method

--- src/main/java/com/nephest/battlenet/sc2/web/service/PendingLadderData.java
+++ src/main/java/com/nephest/battlenet/sc2/web/service/PendingLadderData.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2023 Oleksandr Masniuk
+// Copyright (C) 2020-2024 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.web.service;
@@ -10,19 +10,49 @@ import java.util.concurrent.ConcurrentHashMap;
 public class PendingLadderData
 {
 
-    private final Set<Integer> statsUpdates = ConcurrentHashMap.newKeySet();
-    private final Set<Long> teams = ConcurrentHashMap.newKeySet();
-    private final Set<PlayerCharacter> characters = ConcurrentHashMap.newKeySet();
+    private final Set<Integer> statsUpdates;
+    private final Set<Long> teams;
+    private final Set<PlayerCharacter> characters;
+
+    private PendingLadderData
+    (
+        Set<Integer> statsUpdates,
+        Set<Long> teams,
+        Set<PlayerCharacter> characters
+    )
+    {
+        this.statsUpdates = statsUpdates;
+        this.teams = teams;
+        this.characters = characters;
+    }
+
 
     public PendingLadderData()
     {
+        this
+        (
+            ConcurrentHashMap.newKeySet(),
+            ConcurrentHashMap.newKeySet(),
+            ConcurrentHashMap.newKeySet()
+        );
     }
 
     public PendingLadderData(PendingLadderData data)
     {
+        this();
         this.getStatsUpdates().addAll(data.getStatsUpdates());
         this.getTeams().addAll(data.getTeams());
         this.getCharacters().addAll(data.getCharacters());
+    }
+
+    public static PendingLadderData immutableCopy(PendingLadderData data)
+    {
+        return new PendingLadderData
+        (
+            Set.copyOf(data.statsUpdates),
+            Set.copyOf(data.teams),
+            Set.copyOf(data.getCharacters())
+        );
     }
 
     public void clear()
--- src/test/java/com/nephest/battlenet/sc2/web/service/PendingLadderDataTest.java
+++ src/test/java/com/nephest/battlenet/sc2/web/service/PendingLadderDataTest.java
@@ -1,22 +1,25 @@
-// Copyright (C) 2020-2023 Oleksandr Masniuk
+// Copyright (C) 2020-2024 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.web.service;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.nephest.battlenet.sc2.model.Region;
 import com.nephest.battlenet.sc2.model.local.PlayerCharacter;
 import java.util.List;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class PendingLadderDataTest
 {
 
-    @Test
-    public void testCopyConstructor()
+    @ValueSource(booleans = {true, false})
+    @ParameterizedTest
+    public void testCopyConstructor(boolean immutable)
     {
         PendingLadderData src = new PendingLadderData();
         src.getStatsUpdates().addAll(List.of(1, 3));
@@ -25,7 +28,9 @@ public class PendingLadderDataTest
             new PlayerCharacter(1L, 2L, Region.EU, 3L, 4, "name#1"),
             new PlayerCharacter(2L, 3L, Region.US, 4L, 5, "name#5")
         ));
-        PendingLadderData copy = new PendingLadderData(src);
+        PendingLadderData copy = immutable
+            ? PendingLadderData.immutableCopy(src)
+            : new PendingLadderData(src);
         Assertions.assertThat(copy)
             .usingRecursiveComparison()
             .isEqualTo(src);
@@ -35,13 +40,22 @@ public class PendingLadderDataTest
         assertTrue(src.getStatsUpdates().contains(99));
         assertFalse(copy.getStatsUpdates().contains(99));
 
-        src.clear();
-        assertTrue(src.getStatsUpdates().isEmpty());
-        assertTrue(src.getTeams().isEmpty());
-        assertTrue(src.getCharacters().isEmpty());
-        assertFalse(copy.getStatsUpdates().isEmpty());
-        assertFalse(copy.getTeams().isEmpty());
-        assertFalse(copy.getCharacters().isEmpty());
+        if(immutable)
+        {
+            assertThrows(UnsupportedOperationException.class, ()->copy.getStatsUpdates().clear());
+            assertThrows(UnsupportedOperationException.class, ()->copy.getTeams().clear());
+            assertThrows(UnsupportedOperationException.class, ()->copy.getCharacters().clear());
+        }
+        else
+        {
+            src.clear();
+            assertTrue(src.getStatsUpdates().isEmpty());
+            assertTrue(src.getTeams().isEmpty());
+            assertTrue(src.getCharacters().isEmpty());
+            assertFalse(copy.getStatsUpdates().isEmpty());
+            assertFalse(copy.getTeams().isEmpty());
+            assertFalse(copy.getCharacters().isEmpty());
+        }
     }
 
 }
