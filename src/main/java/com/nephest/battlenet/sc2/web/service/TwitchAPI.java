4eba54f8a39eab8701e7742e6772b8494980181e
nephest <nephestdev@gmail.com>
2024-08-20 13:08:27 +0300

    pull batches with 1 concurrency by default (#69)
    This fixes a bug where an exception is thrown when the batch is too big and thread pool/queue is too small to handle all chunks.

--- src/main/java/com/nephest/battlenet/sc2/web/service/TwitchAPI.java
+++ src/main/java/com/nephest/battlenet/sc2/web/service/TwitchAPI.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2023 Oleksandr Masniuk
+// Copyright (C) 2020-2024 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.web.service;
@@ -24,6 +24,7 @@ import rx.RxReactiveStreams;
 public class TwitchAPI
 {
 
+    public static final int DEFAULT_CONCURRENCY = 1;
     public static final int USER_BATCH_SIZE = 100;
     public static final int STREAM_BATCH_SIZE = 100;
 
@@ -40,24 +41,58 @@ public class TwitchAPI
         return client;
     }
 
-    public Flux<User> getUsersByIds(Set<String> ids)
+    /**
+     * This method accepts arbitrary Reactor concurrency which may be dangerous
+     * if it exceeds Twitch4j thread pool/queue size. Use {@link #getUsersByIds(Set)} with
+     * default concurrency if you don't know a safe value.
+     *
+     * @param ids id set
+     * @param concurrency Reactor concurrency
+     * @return twitch Users
+     * @throws IllegalArgumentException if {@code concurrency} is less than 1
+     */
+    public Flux<User> getUsersByIds(Set<String> ids, int concurrency)
     {
+        if(concurrency < 1) throw new IllegalArgumentException("Concurrency >= 1 required");
+
         return Flux.fromIterable(ids)
             .buffer(USER_BATCH_SIZE)
             .flatMap(idBatch->Flux.from(RxReactiveStreams.toPublisher(
-                client.getHelix().getUsers(null, idBatch, null).toObservable())))
+                client.getHelix().getUsers(null, idBatch, null).toObservable())), concurrency)
+            .map(UserList::getUsers)
+            .flatMapIterable(Function.identity());
+    }
+
+    public Flux<User> getUsersByIds(Set<String> ids)
+    {
+        return getUsersByIds(ids, DEFAULT_CONCURRENCY);
+    }
+
+    /**
+     * This method accepts arbitrary Reactor concurrency which may be dangerous
+     * if it exceeds Twitch4j thread pool/queue size. Use {@link #getUsersByLogins(Set)} with
+     * default concurrency if you don't know a safe value.
+     *
+     * @param logins login set
+     * @param concurrency Reactor concurrency
+     * @return twitch Users
+     * @throws IllegalArgumentException if {@code concurrency} is less than 1
+     */
+    public Flux<User> getUsersByLogins(Set<String> logins, int concurrency)
+    {
+        if(concurrency < 1) throw new IllegalArgumentException("Concurrency >= 1 required");
+
+        return Flux.fromIterable(logins)
+            .buffer(USER_BATCH_SIZE)
+            .flatMap(loginBatch->Flux.from(RxReactiveStreams.toPublisher(
+                client.getHelix().getUsers(null, null, loginBatch).toObservable())), concurrency)
             .map(UserList::getUsers)
             .flatMapIterable(Function.identity());
     }
 
     public Flux<User> getUsersByLogins(Set<String> logins)
     {
-        return Flux.fromIterable(logins)
-            .buffer(USER_BATCH_SIZE)
-            .flatMap(loginBatch->Flux.from(RxReactiveStreams.toPublisher(
-                client.getHelix().getUsers(null, null, loginBatch).toObservable())))
-            .map(UserList::getUsers)
-            .flatMapIterable(Function.identity());
+        return getUsersByLogins(logins, DEFAULT_CONCURRENCY);
     }
 
     public Flux<Stream> getStreamsByGameId(String gameId, int limit)
