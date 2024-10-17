7db57d8b41878900ab86756cd8960e134393f4c0
Oleksandr Masniuk <nephestdev@gmail.com>
2024-10-17 13:27:34 +0300

    add EpochSecondToOffsetDateTimeConverter

---
+++ src/main/java/com/nephest/battlenet/sc2/config/convert/jackson/EpochSecondToOffsetDateTimeConverter.java
@@ -0,0 +1,25 @@
+// Copyright (C) 2020-2024 Oleksandr Masniuk
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package com.nephest.battlenet.sc2.config.convert.jackson;
+
+import com.fasterxml.jackson.databind.util.StdConverter;
+import com.nephest.battlenet.sc2.model.util.SC2Pulse;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+public class EpochSecondToOffsetDateTimeConverter
+extends StdConverter<Long, OffsetDateTime>
+{
+
+    @Override
+    public OffsetDateTime convert(Long l)
+    {
+        return SC2Pulse.offsetDateTime(OffsetDateTime.ofInstant(
+            Instant.ofEpochSecond(l),
+            ZoneOffset.UTC
+        ));
+    }
+
+}
