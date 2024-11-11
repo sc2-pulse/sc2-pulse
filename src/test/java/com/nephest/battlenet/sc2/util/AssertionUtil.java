570064246059a08f1edf9be59681a9ea0c0c0e94
Oleksandr Masniuk <nephestdev@gmail.com>
2024-11-11 11:48:35 +0200

    add AssertionUtil

---
+++ src/test/java/com/nephest/battlenet/sc2/util/AssertionUtil.java
@@ -0,0 +1,32 @@
+// Copyright (C) 2020-2024 Oleksandr Masniuk
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package com.nephest.battlenet.sc2.util;
+
+import java.util.List;
+import java.util.function.BiPredicate;
+
+/**
+ * This class is intended to be used with {@link org.assertj.core.api.Assertions} or similar libs,
+ * specifically in custom comparisons such as
+ * {@link org.assertj.core.api.RecursiveComparisonAssert#withEqualsForFields(BiPredicate, String...)}
+ *
+ */
+public final class AssertionUtil
+{
+
+    private AssertionUtil(){}
+
+    @SuppressWarnings("unchecked")
+    public static boolean numberListEquals(Object l, Object r)
+    {
+        List<Long> lLong = ((List<Number>) l).stream()
+            .map(Number::longValue)
+            .toList();
+        List<Long> rLong = ((List<Number>) r).stream()
+            .map(Number::longValue)
+            .toList();
+        return lLong.equals(rLong);
+    }
+
+}
