913dc6e7549089a5578965ceda6d3b65f9511c86
Oleksandr Masniuk <nephestdev@gmail.com>
2025-07-15 19:20:13 +0300

    add toon handle support

--- src/main/java/com/nephest/battlenet/sc2/model/PlayerCharacterNaturalId.java
+++ src/main/java/com/nephest/battlenet/sc2/model/PlayerCharacterNaturalId.java
@@ -6,9 +6,28 @@ package com.nephest.battlenet.sc2.model;
 public interface PlayerCharacterNaturalId
 {
 
+    String TOON_HANDLE_REGEXP = "^([1235])-S2-(\\d+)-(\\d+)$";
+    String TOON_HANDLE_DESCRIPTION = "regionId-S2-characterRealm-characterBattlenetId";
+
     static PlayerCharacterNaturalId of(Region region, Integer realm, Long battleNetId)
     {
         return new PlayerCharacterNaturalIdImpl(region, realm, battleNetId);
+    }
+
+    static PlayerCharacterNaturalId ofToonHandle(String toonHandle)
+    {
+        String[] split = toonHandle.split("-");
+        if(split.length != 4)
+            throw new IllegalArgumentException("4 section expected: " + toonHandle);
+        if(!split[1].equals("S2"))
+            throw new IllegalArgumentException("The second section must be S2: " + toonHandle);
+
+        return new PlayerCharacterNaturalIdImpl
+        (
+            Region.from(Integer.parseInt(split[0])),
+            Integer.parseInt(split[2]),
+            Long.parseLong(split[3])
+        );
     }
 
     Region getRegion();
@@ -22,6 +41,11 @@ public interface PlayerCharacterNaturalId
         return "/" + getRegion().getId()
             + "/" + getRealm()
             + "/" + getBattlenetId();
+    }
+
+    default String toToonHandle()
+    {
+        return getRegion().getId() + "-S2-" + getRealm() + "-" + getBattlenetId();
     }
 
 }
--- src/test/java/com/nephest/battlenet/sc2/model/PlayerCharacterNaturalIdTest.java
+++ src/test/java/com/nephest/battlenet/sc2/model/PlayerCharacterNaturalIdTest.java
@@ -1,10 +1,17 @@
-// Copyright (C) 2020-2023 Oleksandr Masniuk
+// Copyright (C) 2020-2025 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.nephest.battlenet.sc2.util.TestUtil;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class PlayerCharacterNaturalIdTest
 {
@@ -19,6 +26,43 @@ public class PlayerCharacterNaturalIdTest
         (
             PlayerCharacterNaturalId.of(region, realm, id),
             PlayerCharacterNaturalId.of(region, realm, id)
+        );
+    }
+
+
+
+    @CsvSource
+    ({
+        "1-S2-2-33, US, 2, 33",
+        "2-S2-11-12345, EU, 11, 12345"
+    })
+    @ParameterizedTest
+    public void testToonHandle(String toonHandle, Region region, int realm, long id)
+    {
+        assertTrue(toonHandle.matches(PlayerCharacterNaturalId.TOON_HANDLE_REGEXP));
+        PlayerCharacterNaturalId originalId = PlayerCharacterNaturalId.of(region, realm, id);
+        PlayerCharacterNaturalId toonId = PlayerCharacterNaturalId.ofToonHandle(toonHandle);
+        assertEquals(originalId, toonId);
+        assertEquals(toonHandle, originalId.toToonHandle());
+        assertEquals(toonHandle, toonId.toToonHandle());
+    }
+
+    @CsvSource
+    ({
+        "1-s2-2-33, The second section must be S2",
+        "9-S2-2-33, Invalid id",
+        "1-S2-qwerty-33, For input string: \"qwerty\"",
+        "1-S2-2-qwerty, For input string: \"qwerty\"",
+    })
+    @ParameterizedTest
+    public void testInvalidToonHandle(String toonHandle, String errorMsg)
+    {
+        assertFalse(toonHandle.matches(PlayerCharacterNaturalId.TOON_HANDLE_REGEXP));
+        assertThrows
+        (
+            IllegalArgumentException.class,
+            ()->PlayerCharacterNaturalId.ofToonHandle(toonHandle),
+            errorMsg
         );
     }
 
