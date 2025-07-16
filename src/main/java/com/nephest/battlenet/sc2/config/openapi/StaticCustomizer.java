dcb6649d763520a6fa0f9e8e33a07d1028419cf1
Oleksandr Masniuk <nephestdev@gmail.com>
2025-07-16 19:26:06 +0300

    use consumers instead of schema types

--- src/main/java/com/nephest/battlenet/sc2/config/openapi/StaticCustomizer.java
+++ src/main/java/com/nephest/battlenet/sc2/config/openapi/StaticCustomizer.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2020-2024 Oleksandr Masniuk
+// Copyright (C) 2020-2025 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.config.openapi;
@@ -7,13 +7,14 @@ import com.nephest.battlenet.sc2.util.SpringUtil;
 import io.swagger.v3.oas.models.parameters.Parameter;
 import java.util.Locale;
 import java.util.Map;
+import java.util.function.Consumer;
 import org.springdoc.core.customizers.ParameterCustomizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 
 /**
- * Rewrites types of object parameters based on pre-defined {@link #CLASS_TYPES}. Used for
+ * Customizes parameters based on pre-defined {@link #CLASS_CUSTOMIZERS}. Used for
  * converters that are not picked up by SpringDoc automatically.
  */
 @Component
@@ -21,7 +22,10 @@ public class StaticCustomizer
 implements ParameterCustomizer
 {
 
-    public static final Map<Class<?>, String> CLASS_TYPES = Map.of(Locale.class, "string");
+    public static final Map<Class<?>, Consumer<Parameter>> CLASS_CUSTOMIZERS = Map.of
+    (
+        Locale.class, parameter->OpenApiUtil.getSchema(parameter).setType("string")
+    );
 
     @Autowired
     public StaticCustomizer(){}
@@ -29,10 +33,11 @@ implements ParameterCustomizer
     @Override
     public Parameter customize(Parameter parameter, MethodParameter methodParameter)
     {
-        String type = CLASS_TYPES.get(SpringUtil.getClass(methodParameter));
-        if(type == null) return parameter;
+        Consumer<Parameter> customizer = CLASS_CUSTOMIZERS
+            .get(SpringUtil.getClass(methodParameter));
+        if(customizer == null) return parameter;
 
-        OpenApiUtil.getSchema(parameter).setType(type);
+        customizer.accept(parameter);
         return parameter;
     }
 
