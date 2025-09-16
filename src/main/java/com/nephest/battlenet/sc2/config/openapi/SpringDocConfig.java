791b16653950e2440ea6605d8641bcce5100163c
Oleksandr Masniuk <nephestdev@gmail.com>
2025-09-16 10:40:44 +0300

    clarify teamLegacyId format

--- src/main/java/com/nephest/battlenet/sc2/config/openapi/SpringDocConfig.java
+++ src/main/java/com/nephest/battlenet/sc2/config/openapi/SpringDocConfig.java
@@ -90,7 +90,10 @@ public class SpringDocConfig
                     `queueId-teamTypeId-regionId-legacyId`
                     ### teamLegacyId
                     `member1Realm.member1BnetId.member1Race~memberNRealm.memberNBnetId.memberNRace`
-                    The wildcard race `*` can be used for 1v1 mode teams.
+                    * Team members must be sorted by realm and bnet id in ascending order.
+                    * Only 1v1 teams have races, the race is null in other team formats,
+                    i.e. `member1Realm.member1BnetId.`
+                    * The wildcard race `*` can be used for 1v1 mode teams.
                     ## Rate limit
                     See `RateLimit-Limit` HTTP header to check current rate limits. The limit
                     refreshes every second. Other HTTP headers are not supported.
