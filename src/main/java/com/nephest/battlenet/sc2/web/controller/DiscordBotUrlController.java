ed4d47efae41cf8c67259236faf870518b25a450
Oleksandr Masniuk <nephestdev@gmail.com>
2026-01-05 17:20:39 +0200

    make discord bot invite url optional

--- src/main/java/com/nephest/battlenet/sc2/web/controller/DiscordBotUrlController.java
+++ src/main/java/com/nephest/battlenet/sc2/web/controller/DiscordBotUrlController.java
@@ -1,15 +1,20 @@
-// Copyright (C) 2020-2025 Oleksandr Masniuk
+// Copyright (C) 2020-2026 Oleksandr Masniuk
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 package com.nephest.battlenet.sc2.web.controller;
 
+import com.nephest.battlenet.sc2.discord.Discord;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller @Hidden
-public class ShortURLController
+@Discord
+@Controller
+@Hidden
+@ConditionalOnProperty(name = "com.nephest.battlenet.sc2.discord.bot.invite.url")
+public class DiscordBotUrlController
 {
 
     @Value("${com.nephest.battlenet.sc2.discord.bot.invite.url}")
